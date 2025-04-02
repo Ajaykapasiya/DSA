dac684a6c9b43a29f9fad75dad4805c67dd2f7a2
ajay <ajaykumar290904@gmail.com>
2025-04-03 00:34:11 +0530

    Add implementation to find the single non-duplicate element in a sorted array

---
+++ Search Single Element in a sorted array.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+int main()
+{
+    vector<int> arr = {1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6};
+    int ans = singleNonDuplicate(arr);
+    cout << "The single element is: " << ans << "\n";
+    return 0;
+}
