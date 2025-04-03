bee74552f1576054704a17a102dfa56a60c45901
ajay <ajaykumar290904@gmail.com>
2025-04-03 21:20:31 +0530

    Fix Nth root calculation logic and correct variable declaration in binary search implementation

--- Nth Root of a Number using Binary Search.cpp
+++ Nth Root of a Number using Binary Search.cpp
@@ -3,19 +3,39 @@ using namespace std;
 
 int func(int mid , int n ,int m){
     long long ans = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+       ans = ans * mid;
+       if (mid > m) return 2;
+    }
+    if (mid == m) return 1;
+     return 0;
     
 }
 
 
 int NthRoot(int n , int m ){
     int low = 1;
-    iny high = m;
-    while (low<=high)
+    int high = m;
+    while (low <= high)
     {
         int mid = (low+high)/2;
-        int midN = func(mid , n , m)
+        int midN = func(mid , n , m);
+        if (midN == 1)
+        {
+            return mid;
+        }
+        else if (midN == 0)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = low - 1;
+        }
     }
-    
+    return -1;
 }
 
 
