967f0cdced0e6353be83b83a1fdfffb3a855b2aa
ajay <ajaykumar290904@gmail.com>
2025-04-04 18:47:12 +0530

    Add implementation for finding the smallest divisor given a threshold

---
+++ Find the Smallest Divisor Given a Threshold.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int limit = 8;
+    int ans = smallestDivisor(arr, limit);
+    cout << "The minimum divisor is: " << ans << "\n";
+    return 0;
+}
