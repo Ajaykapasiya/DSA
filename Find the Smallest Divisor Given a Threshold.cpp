831714a4d5da84a977a7d1b61afd54e56f7b102e
ajay <ajaykumar290904@gmail.com>
2025-04-04 18:53:19 +0530

    Add initial implementation of smallest divisor function with binary search

--- Find the Smallest Divisor Given a Threshold.cpp
+++ Find the Smallest Divisor Given a Threshold.cpp
@@ -1,6 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int 
+
+
+
+int smallestDivisor(vector<int> &arr , int limit){
+    int n = arr.size();
+    if(n > limt )return -1;
+
+    int low = 1;
+    int high = *max_element(arr.begin() , arr.end());
+
+    while (low<=high)
+    {
+        int mid = (low+high)/2;
+        if (sumByD(arr,mid) <= limit){
+
+        }
+    }
+    
+}
+
 
 
 
