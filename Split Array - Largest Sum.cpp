5444704263a5bc425a0ed88eb9f70af87351bafb
ajay <ajaykumar290904@gmail.com>
2025-04-05 14:21:47 +0530

    Add countPartitions function to calculate the number of partitions for a given max sum in largest subarray sum minimized algorithm

--- Split Array - Largest Sum.cpp
+++ Split Array - Largest Sum.cpp
@@ -1,6 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+int countPartitions(vector<int> &a, int maxSum){
+    int n = a.size();
+    int partitions = 1;
+    long long subarraySum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (subarraySum + a[i] <= maxsum )
+        {
+            subarraySum  += a[i];
+        }
+        else{
+            partitions++;
+            subarraySum = a[i];
+        }
+    }
+    return partitions;
+    
+}
+
 int largestSubarraySumMinimized(vector<int> &a , int k){
     int low = *max_element(a.begin() , a.end());
     int high = accumulate(a.begin() , a.end() , 0);
@@ -8,10 +29,20 @@ int largestSubarraySumMinimized(vector<int> &a , int k){
     while (low<=high)
     {
         int mid = (low+high)/2;
-        int partitions = countPartitions(a , mid)
-    }
+        int partitions = countPartitions(a , mid);
     
+    if (partitions > k) {
+        low = mid + 1;
+    }
+    else {
+        high = mid - 1;
+    }
+    return low;
 }
+
+
+    
+
 
 
 int main()
