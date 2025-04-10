0caea9908670cbcc151957d7ef3ccc816f2a7733
ajay <ajaykumar290904@gmail.com>
2025-04-10 15:46:27 +0530

    Fix commented-out code in median function to ensure proper functionality

--- Median of Two Sorted Arrays of different sizes.cpp
+++ Median of Two Sorted Arrays of different sizes.cpp
@@ -18,10 +18,10 @@ double median(vector<int>&a , vector<int>&b){
 
         int l1 = INT_MIN , l2 = INT_MIN;
         int r1 = INT_MAX , r2 = INT_MAX;
-        //if (mid1 < n1) r1 = a[mid1];
-        //if (mid2 < n2) r2 = b[mid2];
-        //if (mid1 - 1 >= 0) l1 = a[mid1 - 1];
-        //if (mid2 - 1 >= 0) l2 = b[mid2 - 1];
+        if (mid1 < n1) r1 = a[mid1];
+        if (mid2 < n2) r2 = b[mid2];
+        if (mid1 - 1 >= 0) l1 = a[mid1 - 1];
+        if (mid2 - 1 >= 0) l2 = b[mid2 - 1];
 
 
         if (l1<=r2 && l2<=r1)
